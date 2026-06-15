fd7d99e0c1d0caa75a676b12541e420f94f171f2
Dieter Weber <d.weber@fz-juelich.de>
2026-06-15 16:57:15 +0200

    Robustness; better display
    Convert a number of exceptions to warnings. I don't know what
    is causing the issues, but there seems to be some mismatch between expected and
    received messages. Skipping in case of discrepancies seems to work so
    that the demo continues looping.
    
    Give the display in Rerun unique names. Otherwise the channels
    in the default view get pretty mixed up. It seems to remember
    what keys it has seen for a name in the past.

--- prototypes/run_ssped.sh
+++ prototypes/run_ssped.sh
@@ -7,5 +7,5 @@ trap 'trap - SIGTERM && kill -- -$$ && kill $(jobs -p)' SIGINT SIGTERM EXIT
 # FIXME real example data
 
 python live_server.py merlin.toml acquisition_parameters_SSPED.toml &
-python live_virtual_detectors.py &
+python live_virtual_detectors.py "SSPED live server demo" &
 libertem-live-mib-sim ~/er-c-data/adhoc/livespedescan/SSPED/SPED_256x256_22x22_10186nm_NBD_a5_spot1nm_CL20cm_125msExp_3000msFB_subframing_x8_pivotoff_01/default.hdr
