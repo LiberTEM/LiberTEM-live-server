b27bd72a1cfa26f1e437e0a7059a53c5dd6bd9ac
Dieter Weber <d.weber@fz-juelich.de>
2026-08-20 16:09:23 +0200

    file provided by Sivert

--- prototypes/run_ssped.sh
+++ prototypes/run_ssped.sh
@@ -8,4 +8,4 @@ trap 'trap - SIGTERM && kill -- -$$ && kill $(jobs -p)' SIGINT SIGTERM EXIT
 
 python live_server.py merlin.toml acquisition_parameters_SSPED.toml &
 python live_virtual_detectors.py "SSPED live server demo" &
-libertem-live-mib-sim ~/er-c-data/adhoc/livespedescan/SSPED/SPED_256x256_22x22_10186nm_NBD_a5_spot1nm_CL20cm_125msExp_3000msFB_subframing_x8_pivotoff_01/default.hdr
+libertem-live-mib-sim ~/er-c-data/adhoc/livespedescan/SSPED/by_sivert/20230523_200538/230523_Fe-Joakim_D_Scan9.hdr
