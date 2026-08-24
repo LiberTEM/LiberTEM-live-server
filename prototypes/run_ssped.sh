510215fce8a258d05f960e6be0b229e4b6ca5eb1
Dieter Weber <d.weber@fz-juelich.de>
2026-08-24 16:56:57 +0200

    Work on downsampled data

--- prototypes/run_ssped.sh
+++ prototypes/run_ssped.sh
@@ -8,4 +8,4 @@ trap 'trap - SIGTERM && kill -- -$$ && kill $(jobs -p)' SIGINT SIGTERM EXIT
 
 python live_server.py merlin.toml acquisition_parameters_SSPED.toml &
 python live_virtual_detectors.py "SSPED live server demo" &
-libertem-live-mib-sim ~/er-c-data/adhoc/livespedescan/SSPED/by_sivert/20230523_200538/230523_Fe-Joakim_D_Scan9.hdr
+libertem-live-mib-sim ~/er-c-data/adhoc/livespedescan/demo/SSPED/230523_Fe-Joakim_D_Scan9_excerpt/default.hdr
